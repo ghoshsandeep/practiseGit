a4bb88b22f1d61e7d6b7134c088a2600703717e7
[ghoshsandeep] <[ghosh0133@gmail.com]>
2024-07-21 22:37:56 +0530

    add changes in index.py

--- index.py
+++ index.py
@@ -1,6 +1,14 @@
 class myclass:
+    
     def __init__(self,n1,n2):
-        num1=n1
-        num2=n2
+        self.num1=n1
+        self.num2=n2
         
-        
+    def SumNum(self):
+        return self.num1 + self.num2
+    
+
+
+m1=myclass(4,5)
+result=m1.SumNum()
+print("the result is {0}".format(result)) 
